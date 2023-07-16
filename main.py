53f48bbc64f7da21d7a82955091aba7634feb65b
SatoshiKazuma <sairamkumarm21@gmail.com>
2023-07-16 12:47:01 +0530

    added readme

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def deposit():
     '''collects deposits for later use'''
-    while(True):
+    while (True):
         amount = input("Enter a deposit amount: $")
-        if amount.isdigit() :
+        if amount.isdigit() and amount > 0:
             amount = int(amount)
             return amount
         else:
             print(f"{amount} not valid")
+
+
 deposit()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# SLOTMACHINE - CLI
+
+Built with python, slotmachine-cli simulates the actions of a simple slot machine with 3 "slot lines", and was created with the intention to practice python code.
