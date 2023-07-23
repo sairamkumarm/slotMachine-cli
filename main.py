824c54361eebfb43c6711ad3d1837e17b2532e83
SatoshiKazuma <sairamkumarm21@gmail.com>
2023-07-23 15:53:42 +0530

    idk

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
+MAX_LINES =3
+MAX_BET = 1000
+MIN_BET = 1
+
+def validateInput(label, minVal=1, maxVal=10000):
+    prefix = "$" if (label != "lines") else ""
+    while True:
+        value = input(f"Enter the {label} for betting" + f" [{prefix}{minVal} - {prefix}{maxVal}]" + f": {prefix}")
+        if value.isdigit():
+            value = int(value)
+            if minVal <= value <= maxVal:
+                print(f"{prefix}{value} accepted as {label}")
+                return value
+            else:
+                print(f"{label} must between {minVal} and {maxVal}")
+        else:
+            print(f"{value} is not a valid input for {label}")
+
+
 def deposit():
     '''collects deposits for later use'''
-    while (True):
-        amount = input("Enter a deposit amount: $")
-        if amount.isdigit() and amount > 0:
-            amount = int(amount)
-            return amount
-        else:
-            print(f"{amount} not valid")
+    depositAmount = validateInput("deposit amount", maxVal=MAX_BET)
+    return depositAmount
 
 
-deposit()
+def takebets(accountBalance):
+    lines = validateInput("lines", maxVal=3)
+    betPerLine = validateInput("bet amount", maxVal=accountBalance)
+    betAmount = lines * betPerLine
+    return betAmount
+
+def main():
+    balance = deposit()
+    bet = takebets(balance)
+    balance -= bet
+    return 0
+
+
+main()
